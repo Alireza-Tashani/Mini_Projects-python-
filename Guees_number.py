aa5dcb386840f348796146b295842f6a6bcf66ca
AlirezaTashani <alirezatashanipr@gmail.com>
2025-11-06 16:25:48 +0330

    add user is file

--- Guees_number.py
+++ Guees_number.py
@@ -1,6 +1,33 @@
 import random
+import csv
+users = []
 
 
+def commit():
+    with open('data.csv' , 'w' ,newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(users)
+def add_user(username):
+    users.append([username,0])
+    commit()
+def check_user(username):
+    for i in users:
+        if i[0] == username:
+            return False
+    return True
+def sign_up(username):
+
+    if check_user(username):
+       
+
+        add_user(username)
+        
+
+def read_users():
+    with open('data.csv' , 'r' , newline='')as f :
+        reader = csv.reader(f)
+        return list(reader)
+    
 
 def game():
     random_int = random.randint(0,101)
@@ -19,8 +46,13 @@ def game():
             print(f'you won number was {random_int} in {chance} time')
 
             break
+
 def main():
-    game()
+    global users
+    users = read_users()
+    username = input('enter your name (username for game) : ')
+    sign_up(username)
+    # game()
 
 
 if __name__ == "__main__":
