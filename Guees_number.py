b836283189586cd4c30df14d184bf924a9b71db2
AlirezaTashani <alirezatashanipr@gmail.com>
2025-11-06 16:06:13 +0330

    def main , game

--- Guees_number.py
+++ Guees_number.py
@@ -1,18 +1,27 @@
 import random
 
-random_int = random.randint(0,101)
-chance = 0
 
-while True:
-    input_usr = int(input('enter your number : '))
-    if random_int> input_usr:
-        print('number is bigger')
-        chance+=1
-    elif random_int<input_usr:
-        print('number is lower')
-        chance+=1
-    else:
-        chance+=1
-        print(f'you won number was {random_int} in {chance} time')
-        
-        break
+
+def game():
+    random_int = random.randint(0,101)
+    chance = 0
+
+    while True:
+        input_usr = int(input('enter your number : '))
+        if random_int> input_usr:
+            print('number is bigger')
+            chance+=1
+        elif random_int<input_usr:
+            print('number is lower')
+            chance+=1
+        else:
+            chance+=1
+            print(f'you won number was {random_int} in {chance} time')
+
+            break
+def main():
+    game()
+
+
+if __name__ == "__main__":
+    main()
